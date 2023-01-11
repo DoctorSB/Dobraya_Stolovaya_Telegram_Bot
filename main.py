f9cf4eadf7dec075c1a55665ee2b621786376135
Klotz Galtry <klotzgal@ev-j6.kzn.21-school.ru>
2023-01-11 03:13:44 +0300

    словарь списков

--- main.py
+++ main.py
@@ -3,20 +3,34 @@ import requests
 import json
 from dowloader import Dowloader
 
+
 class Parser:
 
     def __init__(self, sourse):
         self.sourse = sourse
-    
+
     def parse(self):
-        soup = bs.BeautifulSoup(self.sourse, 'html.parser').find('table', class_='table table__menu')
+        soup = bs.BeautifulSoup(self.sourse, 'html.parser').find(
+            'table', class_='table table__menu')
         names = soup.find_all('tr')[1]
-        
+
         # for tr in names:
-        print(names)
-        
-       
-        
+        print(names['class'][0])
+        data = {}
+        cur_key = ''
+        while names.find_next_sibling('tr'):
+
+            if 'table__area' in names['class']:
+                cur_key = names.text
+                # print(cur_key, end=' ')
+                data[cur_key] = []
+            else:
+                data[cur_key] += [names['data-name']]
+
+            # print(names['class'])
+            names = names.find_next_sibling('tr')
+        print(data)
+
 
 if __name__ == '__main__':
     url = 'https://dobraya.su/menu/'
